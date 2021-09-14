125a19fe722b551a1b54e63c0cd332a920502434
embla.froberg <emblafroberg@yahoo.se>
2021-09-14 23:12:14 +0200

    UIWork

---
+++ FaunaSphere Sunrise Project/Assets/GamePlay/Scripts/ChangeScene.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeScene : MonoBehaviour
+{
+   
+    public void ChangeToScene (int sceneToChangeTo)
+    {
+        Application.LoadLevel(sceneToChangeTo);
+    }
+}
