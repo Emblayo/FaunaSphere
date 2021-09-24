e7e01c89dfd0d97d6dad858f858b125a8f210f78
embla.froberg <emblafroberg@yahoo.se>
2021-09-24 12:26:30 +0200

    Changing the fauna based on its tag

---
+++ FaunaSphere Sunrise Project/Assets/GamePlay/Scripts/ChangeFaunaOnTag.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeFaunaOnTag : MonoBehaviour
+{
+    public GameObject Sniffer;
+    public GameObject Hoofer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (gameObject.CompareTag("Sniffer"))
+        {
+            //Debug.Log("This is a Sniffer");
+            Sniffer.SetActive(true);
+            Hoofer.SetActive(false);
+        }
+
+        if (gameObject.CompareTag("Hoofer"))
+        {
+            //Debug.Log("This is a Hoofer");
+            Sniffer.SetActive(false);
+            Hoofer.SetActive(true);
+        }
+
+    }
+}
