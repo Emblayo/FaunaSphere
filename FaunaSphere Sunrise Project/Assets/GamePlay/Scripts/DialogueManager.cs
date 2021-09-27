2b4cdacd12f8c08380080c5e3542b3b77475d79c
embla.froberg <emblafroberg@yahoo.se>
2021-09-27 22:30:10 +0200

    Added dialogue system!

---
+++ FaunaSphere Sunrise Project/Assets/GamePlay/Scripts/DialogueManager.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DialogueManager : MonoBehaviour
+{
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI dialogueText;
+
+    public Animator animator;
+
+    private Queue<string> sentences;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    //starting dialogue
+    public void StartDialogue (Dialogue dialogue)
+    {
+        animator.SetBool("IsOpen", true);
+
+        nameText.text = dialogue.name;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    //displaying the next sentence
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+
+        //calling in the IEnumerator
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    //animating in each letter
+    IEnumerator TypeSentence (string sentence)
+    {
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+
+    //ending dialogue
+    void EndDialogue()
+    {
+        Debug.Log("End of conversation.");
+        animator.SetBool("IsOpen", false);
+    }
+
+}
